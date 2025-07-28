1929fe9d854814d2c1b0a375d5ef531b8049c820
florianschoffke <florian.schoffke@gematik.de>
2025-07-28 12:57:27 +0200

    change common script in comment

--- input/content/assets/js/heading-links.js
+++ input/content/assets/js/heading-links.js
@@ -1,5 +1,5 @@
 // === AUTO-GENERATED FILE ===
-// Do not edit by hand; edit scripts/generate-heading-links-js.sh and config.common.sh instead.
+// Do not edit by hand; edit scripts/generate-heading-links-js.sh and config.sh instead.
 
 const newIssueGithubLink = "https://github.com/gematik/spec-E-Rezept-ServiceRequest/issues/new?template=3-BUG-IG-REPORT.yml";
 // TODO: make repo and parameters configurable
